b93c8e746939f7ac7e2d0ee119ca776a37a3320f
hazemelfeky <hazemelfeky25@gmail.com>
2024-02-12 21:29:56 +0200

    feat: init shared select input

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,32 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import SharedSelect from "./components/SharedSelect.vue";
+
+const form = ref({
+  main: null,
+});
+
+const mainOptions = [
+  { value: "cars", name: "Cars" },
+  { value: "Motocycles", name: "Motocycles" },
+  { value: "Banana", name: "Banana" },
+];
+
+const handleSubmit = () => {
+  console.log(form.value);
+};
+</script>
 
 <template>
-  <div>
-    <h1>Hello</h1>
-  </div>
+  <form @submit.prevent="handleSubmit">
+    <h1>Form</h1>
+    <SharedSelect
+      :form="form"
+      name="main"
+      label="label*"
+      :options="mainOptions"
+      required
+    />
+    <button>Submit</button>
+  </form>
 </template>
---
+++ src/components/SharedSelect.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  form: { type: Object, default: () => ({}) },
+  name: { type: String, default: "value" },
+  label: { type: String, default: "value" },
+  itemName: { type: String, default: "value" },
+  itemValue: { type: String, default: "value" },
+  options: { type: Array, default: () => [] },
+});
+</script>
+<template>
+  <div>
+    <label :for="name">
+      <div>{{ label }}</div>
+      <select :id="name" v-model="form[name]" v-bind="$attrs">
+        <option
+          v-for="option in options"
+          :key="option[itemName]"
+          :value="option[itemName]"
+        >
+          {{ option[itemValue] }}
+        </option>
+      </select>
+    </label>
+  </div>
+</template>
